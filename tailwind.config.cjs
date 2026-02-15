79ead929af453c197816e4761750139f1e8f1b1d
Tony Edwards <tonyedwardspz@users.noreply.github.com>
2026-02-15 12:04:06 +0000

    Adds newsletter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: 'rgb(var(--color-brand-light) / <alpha-value>)',
+          dark: 'rgb(var(--color-brand-dark) / <alpha-value>)',
+          extra: 'rgb(var(--color-brand-extra) / <alpha-value>)',
+          extraDark: 'rgb(var(--color-brand-extraDark) / <alpha-value>)',
+          DEFAULT: 'rgb(var(--color-brand) / <alpha-value>)',
+        },
+      },
+      fontFamily: {
+        header: ['ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
