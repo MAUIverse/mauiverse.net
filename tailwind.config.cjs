8099ecfa243c8bb2fff79f7215d62793baf0f27f
Tony Edwards <tonyedwardspz@users.noreply.github.com>
2026-02-17 19:01:12 +0000

    Design stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -11,6 +12,15 @@ module.exports = {
           extraDark: 'rgb(var(--color-brand-extraDark) / <alpha-value>)',
           DEFAULT: 'rgb(var(--color-brand) / <alpha-value>)',
         },
+        surface: {
+          DEFAULT: 'rgb(var(--color-surface) / <alpha-value>)',
+          alt: 'rgb(var(--color-surface-alt) / <alpha-value>)',
+        },
+        muted: {
+          DEFAULT: 'rgb(var(--color-muted) / <alpha-value>)',
+          light: 'rgb(var(--color-muted-light) / <alpha-value>)',
+        },
+        edge: 'rgb(var(--color-edge) / <alpha-value>)',
       },
       fontFamily: {
         header: ['ui-sans-serif', 'system-ui', 'sans-serif'],
